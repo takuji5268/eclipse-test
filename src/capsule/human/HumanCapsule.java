963e9a16aa206dd41d3d684c391859c8884b5910
TakujiKomine <60042009+takuji5268@users.noreply.github.com>
2025-01-04 21:13:23 +0900

    学習

---
+++ src/capsule/human/HumanCapsule.java
@@ -0,0 +1,19 @@
+package capsule.human;
+
+public class HumanCapsule {
+	private String name;
+	private int age;
+	
+	public HumanCapsule(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+}
